2dfd4fb956321b82a0092f37dbbdc9ef0d54a243
kjzxzzh <332392761@qq.com>
2017-06-23 18:08:36 +0800

    shiro 并未执行

--- src/main/java/cn/org/cstc/bctest/controller/LoginController.java
+++ src/main/java/cn/org/cstc/bctest/controller/LoginController.java
@@ -1,5 +1,7 @@
 package cn.org.cstc.bctest.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -29,54 +31,12 @@ public class LoginController {
 	}
 
 	@RequestMapping(value="/login",method=RequestMethod.POST)
-	public ModelAndView login(@RequestParam("username")String userName,
-						@RequestParam("password")String password,
-						@RequestParam("code")String code,
-						HttpSession session,
-						RedirectAttributes attributes
-	) 
-	{
-		ModelAndView modelAndView = null;
-		boolean checkResult = true; 
-		User user = userService.getUserByUsername(userName);
-		if(user==null){
-			modelAndView = new ModelAndView(new RedirectView("tologin"));
-			attributes.addFlashAttribute("username",userName);
-			attributes.addFlashAttribute("errorCode", "userNotExists");
-			attributes.addFlashAttribute("errorMsg","该用户名不存在！");			
-			checkResult = false;
-			return modelAndView;
-		}
+	public String login(HttpServletRequest request, Map<String, Object> map	) 	{
+		System.out.println("HomeController.login()");
+		String exception = (String) request.getAttribute("shiroLoginFailure");
+	    System.out.println("exception=" + exception);
+	    return "/login";
 		
-		if(user!=null&&!user.getPassword().equals(password.trim())){
-			modelAndView = new ModelAndView(new RedirectView("tologin"));
-			
-			attributes.addFlashAttribute("username",userName);
-			attributes.addFlashAttribute("errorCode", "invalidPassword");
-			attributes.addFlashAttribute("errorMsg","密码错误！");
-			
-			checkResult = false;
-			return modelAndView;
-		}
-		String sessesionYZM = (String) session.getAttribute("YZM");
-		if(code==null||(code!=null && !code.equals(sessesionYZM))){
-			modelAndView = new ModelAndView(new RedirectView("tologin"));
-			
-			attributes.addFlashAttribute("username",userName);
-			attributes.addFlashAttribute("errorCode", "invalidYZM");
-			attributes.addFlashAttribute("errorMsg","验证码错误！");
-			
-			checkResult = false;
-			return modelAndView;
-		}
-		
-		if(checkResult){
-			session.setAttribute("user", user);
-			modelAndView = new ModelAndView(new RedirectView("account/index"));
-			userService.updateLoginTime(user.getId());
-			return modelAndView;
-		}
-		return modelAndView;
 	}
 	
 	
