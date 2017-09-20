db6b8e406d2e538b1645ae500a6dc0dd917b0ce4
kjzxzzh <332392761@qq.com>
2017-09-20 18:41:12 +0800

    new

--- src/main/java/cn/org/cstc/bctest/bootstrap/Application.java
+++ src/main/java/cn/org/cstc/bctest/bootstrap/Application.java
@@ -11,5 +11,6 @@ public class Application {
     public static void main(String[] args) throws InterruptedException {
     	Object[] sources = {Application.class};
         SpringApplication.run(sources, args);
+        System.out.println("123");
     }
 }
