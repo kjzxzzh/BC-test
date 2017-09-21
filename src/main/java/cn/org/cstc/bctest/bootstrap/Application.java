a8a8ac38ca836e7024f4f81a7e3df1b30cd860a6
kjzxzzh <332392761@qq.com>
2017-09-21 14:07:13 +0800

    version3

--- src/main/java/cn/org/cstc/bctest/bootstrap/Application.java
+++ src/main/java/cn/org/cstc/bctest/bootstrap/Application.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
     public static void main(String[] args) throws InterruptedException {
     	Object[] sources = {Application.class};
-    	System.out.println("version2");
+    	System.out.println("version3");
         SpringApplication.run(sources, args);
         
     }
